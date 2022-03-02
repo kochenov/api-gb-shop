39e21169878f6c7ea51e2014de6576086884db89
kochenov <kochenof@gmail.com>
2022-03-02 22:49:32 +0300

    Добавлены классы, для работы с товарами

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 Route::apiResources([
-    //'product' => ProductController::class,
+    'product' => ProductController::class,
     'category' => CategoryController::class,
     //'cart' => CartController::class,
 ]);
