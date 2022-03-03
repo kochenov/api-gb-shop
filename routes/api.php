dc69da5d835d07a5be08b2163bd3d4aa846155e1
kochenov <kochenof@gmail.com>
2022-03-03 09:02:05 +0300

    Добавлен маршрут для корзины

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -21,5 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::apiResources([
     'product' => ProductController::class,
     'category' => CategoryController::class,
-    //'cart' => CartController::class,
+    'cart' => CartController::class,
 ]);
